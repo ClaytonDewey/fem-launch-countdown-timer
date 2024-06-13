47b22184e4cbc1d49bf78c7c9d401526491f6a4f
ClaytonDewey <clay@dryadmedia.com>
2024-06-13 16:56:13 -0500

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,26 @@
+import { Footer } from '.';
+
+const App = () => {
+  return (
+    <>
+      <main>
+        <h1>We're launching soon</h1>
+
+        <div>
+          <span>Days</span>
+          <span>Hours</span>
+          <span>Minutes</span>
+          <span>Seconds</span>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
+export default App;
+
+/* 
+- [Font Awesome](https://fontawesome.com)
+- [IcoMoon](https://icomoon.io)
+- [Ionicons](https://ionicons.com)
+ */
