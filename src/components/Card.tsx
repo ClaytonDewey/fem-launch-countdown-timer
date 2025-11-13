fa73f19418831becd261b6faee077f3951f3e4fb
ClaytonDewey <clay@dryadmedia.com>
2025-11-13 09:02:12 -0600

    refactor: Rework time display
    Set up keyframes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,9 +5,19 @@ interface CardProps {
 
 const Card = ({ number, label }: CardProps) => {
   return (
-    <div className='card timer__card'>
-      <div className='timer__number'>{number}</div>
-      <div className='timer__label'>{label}</div>
+    <div className='card time__section'>
+      <div className='time__segment'>
+        <div className='segment__display'>
+          <div className='segment__display-top'>{number}</div>
+          <div className='segment__display-bottom'>{number}</div>
+          <div className='segment__overlay'>
+            <div className='segment__overlay-top'>{number}</div>
+            <div className='segment__overlay-bottom'>{number}</div>
+          </div>
+        </div>
+      </div>
+
+      <div className='time__label'>{label}</div>
     </div>
   );
 };
